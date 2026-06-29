9549c230f97ed1ecd807fa27cdef30b8c5db5253
Pravallika <chinnalurupravallika789@gmail.com>
2026-06-29 16:32:21 +0530

    update

--- Frontend/src/ADMIN/Configuration/ConfigSubViewer.js
+++ Frontend/src/ADMIN/Configuration/ConfigSubViewer.js
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
-/* import React from 'react';
-=======
 import React from "react";
 import AcademicYear from "./Academic Year/AcademicYear";
 import BankAccount from "./Bank Account Details";
 import Section from "./Section";
 import SyllabusTypes from "./Syllabus types";
 import Users from './Users/Users';
-import FeeMaster from './Fee Master/FeeMaster'
->>>>>>> 23dadab8849419e060ffbefdc53872d340eaf440
+import FeeMaster from './Fee Master/FeeMaster';
+import UniformSetting from "./Uniform settings/UniformSetting";
+import UniformFee from "./Uniform Fee/UniformFee";
 
 function ConfigSubViewer({ category }) {
   switch (category) {
@@ -24,53 +22,10 @@ function ConfigSubViewer({ category }) {
       return <Users />;
     case 'Fee Master':
       return <FeeMaster />;
-    default:
-      return (
-        <div style={{ padding: "20px" }}>
-          <h3>Configuration: {category}</h3>
-          <p>Configure settings and details for {category}.</p>
-        </div>
-      );
-  }
-}
-
-<<<<<<< HEAD
-export default ConfigSubViewer;
- */
-
-/* import React from "react";
-
-import UniformFee from "./Uniform Fee/UniformFee";
-
-function ConfigSubViewer({ category }) {
-  switch (category) {
-    case "Uniform Fee":
-      return <UniformFee />;
-
-    default:
-      return (
-        <div style={{ padding: "20px" }}>
-          <h3>Configuration: {category}</h3>
-          <p>Configure settings and details for {category}.</p>
-        </div>
-      );
-  }
-}
-
-export default ConfigSubViewer; */
-
-import React from "react";
-import UniformSetting from "./Uniform settings/UniformSetting";
-import UniformFee from "./Uniform Fee/UniformFee";
-
-function ConfigSubViewer({ category }) {
-  switch (category) {
     case "Uniform settings":
       return <UniformSetting />;
-
     case "Uniform Fee":
       return <UniformFee />;
-
     default:
       return (
         <div style={{ padding: "20px" }}>
@@ -82,6 +37,3 @@ function ConfigSubViewer({ category }) {
 }
 
 export default ConfigSubViewer;
-=======
-export default ConfigSubViewer;
->>>>>>> 23dadab8849419e060ffbefdc53872d340eaf440
