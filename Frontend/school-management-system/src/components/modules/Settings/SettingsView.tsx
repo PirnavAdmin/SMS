733eb0266ca12b91e6b17ade25670f1fed0fd322
Gokul4366 <gokulvasantha4366@gmail.com>
2026-09-08 11:34:57 +0530

    braces fix

--- Frontend/school-management-system/src/components/modules/Settings/SettingsView.tsx
+++ Frontend/school-management-system/src/components/modules/Settings/SettingsView.tsx
@@ -536,7 +536,6 @@ export const SettingsView: React.FC = () => {
     setIdForm(nextForm);
     saveIdSequenceSettings(nextForm);
     addToast("success", "Custom Sequence Added", "New custom sequence format added.");
-  };
 
     try {
       await addOrUpdateCustomIdFormatApi(newSeq);
